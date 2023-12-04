bc2b166c68335951ee670cd8975d0024293c0255
bakhtiyargil <bext97@mail.ru>
2023-12-04 23:47:02 +0400

    singlyLinkedList added

--- src/main/java/az/baxtiyargil/datastructuresalgorithms/list/ListMain.java
+++ src/main/java/az/baxtiyargil/datastructuresalgorithms/list/ListMain.java
@@ -1,9 +1,21 @@
 package az.baxtiyargil.datastructuresalgorithms.list;
 
+import az.baxtiyargil.datastructuresalgorithms.list.model.SinglyLinkedList;
 
 public class ListMain {
 
     public static void main(String[] args) {
+        testSinglyLinkedList();
+    }
+
+    public static void testSinglyLinkedList() {
+        var list = new SinglyLinkedList<String>();
+        list.addFirst("Aaa");
+        list.addFirst("Bbb");
+        list.addFirst("Ccc");
+        System.out.println(list.first());
+        list.removeFirst();
+        System.out.println(list.first());
     }
 
 }
