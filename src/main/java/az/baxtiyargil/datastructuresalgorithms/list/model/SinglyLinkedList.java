3f18d0b39e3c64e12b7401a3debb949803f2b371
bakhtiyargil <bext97@mail.ru>
2024-01-02 19:15:23 +0400

    clone method added for SinglyLinkedList

--- src/main/java/az/baxtiyargil/datastructuresalgorithms/list/model/SinglyLinkedList.java
+++ src/main/java/az/baxtiyargil/datastructuresalgorithms/list/model/SinglyLinkedList.java
@@ -1,6 +1,6 @@
 package az.baxtiyargil.datastructuresalgorithms.list.model;
 
-public class SinglyLinkedList<E> {
+public class SinglyLinkedList<E> implements Cloneable {
 
     private Node<E> head;
     private Node<E> tail;
@@ -80,6 +80,22 @@ public class SinglyLinkedList<E> {
             walkB = walkB.getNext();
         }
         return true;
+    }
+
+    public SinglyLinkedList<E> clone() throws CloneNotSupportedException {
+        SinglyLinkedList<E> other = (SinglyLinkedList<E>) super.clone();
+        if (size > 0) {
+            other.head = new Node<>(head.getElement(), null);
+            Node<E> walk = head.getNext();
+            Node<E> otherTail = other.head;
+            while (walk != null) {
+                Node<E> newest = new Node<>(walk.getElement(), null);
+                otherTail.setNext(newest);
+                otherTail = newest;
+                walk = walk.getNext();
+            }
+        }
+        return other;
     }
 
     private static class Node<E> {
