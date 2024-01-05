0cfe1ef2b7b0e341ab070e6930c03ce6fbed1ea4
bakhtiyargil <bext97@mail.ru>
2024-01-05 14:45:59 +0400

    linear recursion methods added

--- src/main/java/az/baxtiyargil/datastructuresalgorithms/recursion/RecursionMain.java
+++ src/main/java/az/baxtiyargil/datastructuresalgorithms/recursion/RecursionMain.java
@@ -3,11 +3,18 @@ package az.baxtiyargil.datastructuresalgorithms.recursion;
 import az.baxtiyargil.datastructuresalgorithms.recursion.implementation.BinarySearch;
 import az.baxtiyargil.datastructuresalgorithms.recursion.implementation.EnglishRuler;
 import az.baxtiyargil.datastructuresalgorithms.recursion.implementation.Factorial;
+import az.baxtiyargil.datastructuresalgorithms.recursion.utility.RecursionImplUtility;
+
+import java.util.Arrays;
 
 public class RecursionMain {
 
     public static void main(String[] args) {
-        binarySearchTesting();
+        powerOptimizedTesting();
+        //powerTesting();
+        //reverseArrayTesting();
+        //linearSumTesting();
+        //binarySearchTesting();
         //englishRulerTesting();
         //factorialTesting();
     }
@@ -26,6 +33,28 @@ public class RecursionMain {
         System.out.println(data.length);
         var exists = BinarySearch.search(data, 7, 0, data.length - 1);
         System.out.println(exists);
+    }
+
+    public static void linearSumTesting() {
+        int[] data = {1, 1, 2, 3};
+        var sum = RecursionImplUtility.linearSum(data, data.length);
+        System.out.println("Sum: " + sum);
+    }
+
+    public static void reverseArrayTesting() {
+        int[] data = {1, 1, 2, 3};
+        RecursionImplUtility.reverseArray(data, 0, data.length - 1);
+        System.out.println("Data: " + Arrays.toString(data));
+    }
+
+    public static void powerTesting() {
+        var power = RecursionImplUtility.power(5, 3);
+        System.out.println("Power: " + power);
+    }
+
+    public static void powerOptimizedTesting() {
+        var power = RecursionImplUtility.powerOptimized(5, 3);
+        System.out.println("Power: " + power);
     }
 
 }
