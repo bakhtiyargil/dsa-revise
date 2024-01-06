47cdcc64813aef2285b4b7d88c494abc3b66c661
bakhtiyargil <bext97@mail.ru>
2024-01-06 23:45:32 +0400

    additional recursive methods

--- src/main/java/az/baxtiyargil/datastructuresalgorithms/recursion/utility/RecursionImplUtility.java
+++ src/main/java/az/baxtiyargil/datastructuresalgorithms/recursion/utility/RecursionImplUtility.java
@@ -35,4 +35,15 @@ public class RecursionImplUtility {
         }
     }
 
+    public static int binarySum(int[] data, int low, int high) {
+        if (low > high) {
+            return 0;
+        } else if (low == high) {
+            return data[low];
+        } else {
+            int mid = (low + high) / 2;
+            return binarySum(data, low, mid) + binarySum(data, mid + 1, high);
+        }
+    }
+
 }
