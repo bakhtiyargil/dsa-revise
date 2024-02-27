dae48e5095879388933a345860f920667de5eca8
bakhtiyargil <bext97@mail.ru>
2024-02-27 23:55:12 +0400

    small additions

--- src/main/java/az/baxtiyargil/datastructuresalgorithms/list/model/FavoritesList.java
+++ src/main/java/az/baxtiyargil/datastructuresalgorithms/list/model/FavoritesList.java
@@ -1,5 +1,9 @@
 package az.baxtiyargil.datastructuresalgorithms.list.model;
 
+import az.baxtiyargil.datastructuresalgorithms.list.positional.LinkedPositionalList;
+import az.baxtiyargil.datastructuresalgorithms.list.positional.Position;
+import az.baxtiyargil.datastructuresalgorithms.list.positional.PositionalList;
+
 public class FavoritesList<E> {
 
     protected static class Item<E> {
@@ -21,7 +25,28 @@ public class FavoritesList<E> {
         public void increment() {
             count++;
         }
-    
+
+    }
+
+    PositionalList<Item<E>> list = new LinkedPositionalList<>();
+
+    public FavoritesList() {
+    }
+
+    protected E value(Position<Item<E>> p) {
+        return p.getElement().getValue();
+    }
+
+    protected int count(Position<Item<E>> p) {
+        return p.getElement().getCount();
+    }
+
+    protected Position<Item<E>> findPosition(E e) {
+        Position<Item<E>> walk = list.first();
+        while (walk != null && !e.equals(value(walk))) {
+            walk = list.after(walk);
+        }
+        return walk;
     }
 
 }
